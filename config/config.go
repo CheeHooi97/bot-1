33f2d063a0b40c1fee0f9d64e6475ba0e9460aba
Chee Hooi Lee <leecheehooi.hengman.solutions>
2025-05-24 12:42:15 +0800

    up

--- config/config.go
+++ config/config.go
@@ -14,10 +14,12 @@ var (
 	TelegramTokenBTC1m  string
 	TelegramTokenBTC5m  string
 	TelegramTokenBTC15m string
-	TelegramTokenBTC30m string
-	TelegramTokenBTC1h  string
-	TelegramTokenBTC4h  string
-	TelegramTokenBTC1d  string
+	// TelegramTokenBTC30m string
+	TelegramTokenBTC1h string
+	TelegramTokenBTC4h string
+	// TelegramTokenBTC1d  string
+	TelegramTokenETH15m string
+	TelegramTokenETH4h  string
 	TelegramChatId      string
 )
 
@@ -31,10 +33,12 @@ func LoadConfig() {
 	TelegramTokenBTC1m = GetEnv("TELEGRAM_TOKEN_BTC_1m")
 	TelegramTokenBTC5m = GetEnv("TELEGRAM_TOKEN_BTC_5m")
 	TelegramTokenBTC15m = GetEnv("TELEGRAM_TOKEN_BTC_15m")
-	TelegramTokenBTC30m = GetEnv("TELEGRAM_TOKEN_BTC_30m")
+	// TelegramTokenBTC30m = GetEnv("TELEGRAM_TOKEN_BTC_30m")
 	TelegramTokenBTC1h = GetEnv("TELEGRAM_TOKEN_BTC_1h")
 	TelegramTokenBTC4h = GetEnv("TELEGRAM_TOKEN_BTC_4h")
-	TelegramTokenBTC1d = GetEnv("TELEGRAM_TOKEN_BTC_1d")
+	// TelegramTokenBTC1d = GetEnv("TELEGRAM_TOKEN_BTC_1d")
+	TelegramTokenETH15m = GetEnv("TELEGRAM_TOKEN_ETH_15m")
+	TelegramTokenETH4h = GetEnv("TELEGRAM_TOKEN_ETH_4h")
 	TelegramChatId = GetEnv("TELEGRAM_CHAT_ID")
 
 }
