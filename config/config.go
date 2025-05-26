afc4b32936b9c7090d118105055dd23f1ee1c248
Chee Hooi Lee <leecheehooi.hengman.solutions>
2025-05-26 17:26:26 +0800

    up

--- config/config.go
+++ config/config.go
@@ -14,17 +14,21 @@ var (
 	TelegramTokenBTC1m  string
 	TelegramTokenBTC5m  string
 	TelegramTokenBTC15m string
-	// TelegramTokenBTC30m string
-	TelegramTokenBTC1h string
-	TelegramTokenBTC4h string
+	TelegramTokenBTC30m string
+	TelegramTokenBTC1h  string
+	TelegramTokenBTC4h  string
 	// TelegramTokenBTC1d  string
 	TelegramTokenETH1m  string
 	TelegramTokenETH5m  string
 	TelegramTokenETH15m string
+	TelegramTokenETH30m string
 	TelegramTokenETH1h  string
 	TelegramTokenETH4h  string
 	// TelegramTokenETH1d  string
 	TelegramTokenSOL1m string
+	TelegramTokenBNB1m string
+	TelegramTokenADA1m string
+	TelegramTokenXRP1m string
 	TelegramChatId     string
 	TelegramTestToken  string
 	TelegramTestToken2 string
@@ -40,16 +44,20 @@ func LoadConfig() {
 	TelegramTokenBTC1m = GetEnv("TELEGRAM_TOKEN_BTC_1m")
 	TelegramTokenBTC5m = GetEnv("TELEGRAM_TOKEN_BTC_5m")
 	TelegramTokenBTC15m = GetEnv("TELEGRAM_TOKEN_BTC_15m")
-	// TelegramTokenBTC30m = GetEnv("TELEGRAM_TOKEN_BTC_30m")
+	TelegramTokenBTC30m = GetEnv("TELEGRAM_TOKEN_BTC_30m")
 	TelegramTokenBTC1h = GetEnv("TELEGRAM_TOKEN_BTC_1h")
 	TelegramTokenBTC4h = GetEnv("TELEGRAM_TOKEN_BTC_4h")
 	// TelegramTokenBTC1d = GetEnv("TELEGRAM_TOKEN_BTC_1d")
 	TelegramTokenETH1m = GetEnv("TELEGRAM_TOKEN_ETH_1m")
 	TelegramTokenETH5m = GetEnv("TELEGRAM_TOKEN_ETH_5m")
 	TelegramTokenETH15m = GetEnv("TELEGRAM_TOKEN_ETH_15m")
+	TelegramTokenETH30m = GetEnv("TELEGRAM_TOKEN_ETH_30m")
 	TelegramTokenETH1h = GetEnv("TELEGRAM_TOKEN_ETH_1h")
 	TelegramTokenETH4h = GetEnv("TELEGRAM_TOKEN_ETH_4h")
 	TelegramTokenSOL1m = GetEnv("TELEGRAM_TOKEN_SOL_1m")
+	TelegramTokenBNB1m = GetEnv("TELEGRAM_TOKEN_BNB_1m")
+	TelegramTokenADA1m = GetEnv("TELEGRAM_TOKEN_ADA_1m")
+	TelegramTokenXRP1m = GetEnv("TELEGRAM_TOKEN_XRP_1m")
 	TelegramChatId = GetEnv("TELEGRAM_CHAT_ID")
 	TelegramTestToken = GetEnv("TELEGRAM_TEST_TOKEN")
 	TelegramTestToken2 = GetEnv("TELEGRAM_TEST_TOKEN2")
