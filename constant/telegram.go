9f05a5a32607b68f7846052026223be974d2d87a
Chee Hooi Lee <leecheehooi.hengman.solutions>
2025-06-10 13:41:12 +0800

    up

--- constant/telegram.go
+++ constant/telegram.go
@@ -3,56 +3,56 @@ package constant
 func GetThreadIdMap() map[string]any {
 	return map[string]any{
 		"btcusdt": map[string]int64{
-			"1m":  12,
-			"5m":  72,
-			"15m": 74,
-			"1h":  76,
-			"4h":  79,
+			"4h": 79,
+			"1d": 76,
 		},
 		"ethusdt": map[string]int64{
-			"1m":  42,
-			"5m":  51,
-			"15m": 57,
-			"1h":  63,
-			"4h":  66,
+			"4h": 66,
+			"1d": 74,
 		},
 		"solusdt": map[string]int64{
-			"1m":  86,
-			"5m":  92,
-			"15m": 95,
-			// "1h": 0,
-			// "4h": 0,
-			// "1d": 0,
+			"4h": 86,
+			"1d": 72,
 		},
 		"bnbusdt": map[string]int64{
-			"1m": 88,
-			// "5m":  0,
-			"15m": 99,
-			// "1h":  0,
-			// "4h":0,
-			// "1d": 0,
+			"4h": 88,
 		},
 		"adausdt": map[string]int64{
-			"1m":  5,
-			"5m":  101,
-			"15m": 104,
-			// "1h": 0,
-			// "4h": 0,
-			// "1d": 0,
+			"4h": 5,
+			"1d": 12,
 		},
 		"xrpusdt": map[string]int64{
-			"1m": 90,
-			// "5m":   0,
-			"15m": 117,
-			// "1h": 0,
-			// "4h":0,
-			// "1d": 0,
-		},
-		"hypeusdt": map[string]int64{
-			"1m": 0,
+			"4h": 90,
 		},
 		"dogeusdt": map[string]int64{
-			"1m": 0,
+			"4h": 117,
+		},
+		"suiusdt": map[string]int64{
+			"1m": 101,
+		},
+		"linkusdt": map[string]int64{
+			"1m": 104,
+		},
+		"avaxusdt": map[string]int64{
+			"1m": 99,
+		},
+		"tonusdt": map[string]int64{
+			"4h": 92,
+		},
+		"dot": map[string]int64{
+			"4h": 95,
+		},
+		"shib1000usdt": map[string]int64{
+			"4h": 42,
+		},
+		"pepeusdt": map[string]int64{
+			"4h": 51,
+		},
+		"tiausdt": map[string]int64{
+			"4h": 57,
+		},
+		"trumpusdt": map[string]int64{
+			"4h": 63,
 		},
 	}
 }
