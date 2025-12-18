f9db372bb9c2214dcd67baeff59374ae0c44e4cc
CheeHooi97 <57720168+CheeHooi97@users.noreply.github.com>
2025-12-18 16:49:37 +0800

    Update symbol.go

--- constant/symbol.go
+++ constant/symbol.go
@@ -5,56 +5,21 @@ import "bot-1/config"
 func GetTokenMap() map[string]any {
 	return map[string]any{
 		"btcusdt": map[string]string{
-			"4h": config.BTC4h,
-			"1d": config.BTC1d,
+			"4h": config.BTCFuture4h,
+			"1h": config.BTCFuture1h,
 		},
 		"ethusdt": map[string]string{
-			"4h": config.ETH4h,
-			"1d": config.ETH1d,
+			"4h": config.ETHFuture4h,
+			"1h": config.ETHFuture1h,
 		},
 		"adausdt": map[string]string{
-			"4h": config.ADA4h,
-			"1d": config.ADA1d,
+			"4h": config.ADAFuture4h,
 		},
 		"solusdt": map[string]string{
-			"4h": config.SOL4h,
-			"1d": config.SOL1d,
+			"4h": config.SOLFuture4h,
 		},
 		"bnbusdt": map[string]string{
-			"4h": config.BNB4h,
-		},
-		"xrpusdt": map[string]string{
-			"4h": config.XRP4h,
-		},
-		"dogeusdt": map[string]string{
-			"4h": config.DOGE4h,
-		},
-		"suiusdt": map[string]string{
-			"4h": config.SUI4h,
-		},
-		"linkusdt": map[string]string{
-			"4h": config.LINK4h,
-		},
-		"avaxusdt": map[string]string{
-			"4h": config.AVAX4h,
-		},
-		"tonusdt": map[string]string{
-			"4h": config.TON4h,
-		},
-		"dotusdt": map[string]string{
-			"4h": config.DOT4h,
-		},
-		"thetausdt": map[string]string{
-			"4h": config.THETA4h,
-		},
-		"wldusdt": map[string]string{
-			"4h": config.WLD4h,
-		},
-		"tiausdt": map[string]string{
-			"4h": config.TIA4h,
-		},
-		"trumpusdt": map[string]string{
-			"4h": config.TRUMP4h,
+			"4h": config.BNBFuture4h,
 		},
 	}
 }
